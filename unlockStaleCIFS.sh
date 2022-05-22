20875fb36da703ea9728a5d5b509f0c32abecc6d
daman20 <79991838+daman20@users.noreply.github.com>
2022-05-21 22:06:20 -0400

    Added logging and permament fix info

--- unlockStaleCIFS.sh
+++ unlockStaleCIFS.sh
@@ -1,2 +1,7 @@
 umount -f -a -t cifs -l
+echo "Unmount Success"
 mount -t cifs
+echo "Remount Sucess"
+echo ""
+echo "--- All Fixed, Although Not Permanetly. For a permanent fix, see the link below---"
+echo "https://forums.unraid.net/topic/103945-psahow-to-proper-fix-for-smb-stale-file-handle/"
